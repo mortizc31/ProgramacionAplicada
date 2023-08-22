190baff70559bc79072eb48c22d3ef15a841b87f
Maria Fernanda Ortiz Corredor <142780447+mortizc31@users.noreply.github.com>
2023-08-22 13:05:49 -0500

    Add files via upload

---
+++ primerosProgramas/For.py
@@ -0,0 +1,9 @@
+import time
+
+n = 20
+
+for i in range (n):
+     print("estudiando el ciclo for")
+     print("i=",i)
+     time.sleep(1)
+     
---
+++ primerosProgramas/While.py
@@ -0,0 +1,5 @@
+import time
+
+while True:
+    print("Revisando el ciclo while")
+    time.sleep(0.5)
